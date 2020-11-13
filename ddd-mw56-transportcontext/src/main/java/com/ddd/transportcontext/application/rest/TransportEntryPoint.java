4ce09cae7b79e5485e75b780fa95c7a83c45b109
sruizvass <sergio.ruiz@vass.es>
2020-11-13 10:36:54 +0100

    Uodate reference

--- ddd-mw56-transportcontext/src/main/java/com/ddd/transportcontext/application/rest/TransportEntryPoint.java
+++ ddd-mw56-transportcontext/src/main/java/com/ddd/transportcontext/application/rest/TransportEntryPoint.java
@@ -1,6 +1,8 @@
 package com.ddd.transportcontext.application.rest;
 
-//@RestController
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TransportEntryPoint {
 
 }
