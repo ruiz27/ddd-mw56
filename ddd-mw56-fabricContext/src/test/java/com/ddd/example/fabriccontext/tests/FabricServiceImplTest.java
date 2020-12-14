8663ecb0dae4521aa76fc09403ac655c7e716e60
sruizvass <sergio.ruiz@vass.es>
2020-12-14 15:47:05 +0100

    update test

--- ddd-mw56-fabricContext/src/test/java/com/ddd/example/fabriccontext/tests/FabricServiceImplTest.java
+++ ddd-mw56-fabricContext/src/test/java/com/ddd/example/fabriccontext/tests/FabricServiceImplTest.java
@@ -5,11 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import com.ddd.example.fabriccontext.model.Box;
 import com.ddd.example.fabriccontext.service.impl.FabricServiceImpl;
+
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 @RunWith(MockitoJUnitRunner.class)
 public class FabricServiceImplTest {
-
 
 	@Before
 	public void init() {
@@ -19,7 +22,9 @@ public class FabricServiceImplTest {
 	@Test
 	public void when_registerProcess_correct() {
 		FabricServiceImpl fac = new FabricServiceImpl();
-		fac.processShipment("GIFT");
+		Mono<Box> res = fac.processShipment("GIFT");
+		StepVerifier.create(res.log())
+		.expectNext(new Box())
+		.verifyComplete();
 	}
 }
-	
